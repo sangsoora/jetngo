3d2e5039675b8728d99313fe948be21b0b32663a
Sang Soo Ra <nadang82@gmail.com>
2020-03-16 18:18:26 +0100

    removed authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :authenticate_user!, only: :home
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # skip_before_action :authenticate_user!, only: :home
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  # def configure_permitted_parameters
+  #   # For additional fields in app/views/devise/registrations/new.html.erb
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
 
-    # For additional in app/views/devise/registrations/edit.html.erb
-  end
+  #   # For additional in app/views/devise/registrations/edit.html.erb
+  # end
 end
