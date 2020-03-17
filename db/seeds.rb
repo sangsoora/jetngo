e91b22574a335acf52fe419d31116008aeb53eaa
Leon Ellerbrock <ellerbrockleon@gmail.com>
2020-03-17 14:05:21 +0100

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake cities...'
+10.times do
+  city = City.new(
+    name:    Faker::Nation.capital_city
+  )
+  city.save!
+end
+puts 'Finished!'
+
+puts 'Creating 10 fake jets...'
+10.times do
+  jet = Jet.new(
+    model:    Faker::name,
+    name: Faker::Company.name,
+    capacity:  rand(4..15),
+    unit_price: rand(1000..10000),
+    city_id: rand(1..10),
+    user_id: rand(1..3)
+  )
+  jet.save!
+end
+puts 'Finished!'
