4f8d4869a9a1d15350e3af0107bfd228ccb1e3c2
Sang Soo Ra <nadang82@gmail.com>
2020-03-17 14:25:06 +0100

    deleted uniqueness for jet

--- app/models/jet.rb
+++ app/models/jet.rb
@@ -2,7 +2,7 @@ class Jet < ApplicationRecord
   belongs_to :user
   belongs_to :city
   has_many :bookings
-  validates :name, uniqueness: true, presence: true, length: { minimum: 3 }
+  validates :name, presence: true, length: { minimum: 3 }
   validates :capacity, presence: true, numericality: true
   validates :unit_price, presence: true, numericality: true
 end
