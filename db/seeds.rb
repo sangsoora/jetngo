5e8b869e8cfa64f3c5d17c3844e4b3e79f3d70d5
Emiliano Dogaru <dogaru.emiliano@gmail.com>
2020-03-19 17:28:48 +0100

    changed the db/seed.rb (added the same cities for every seed)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+puts '*' * 30
+puts "Start seeding"
+
 puts 'Creating 3 users'
 User.create(email: "leon@hotmail.com", password: "lewagon1", first_name: "le", last_name: "ellerb")
 User.create(email: "sangsoo@hotmail.com", password: "lewagon2", first_name: "sangoo", last_name: "ellerbr")
 User.create(email: "abder@hotmail.com", password: "lewagon3", first_name: "abder", last_name: "ellerbrock")
+puts "Done creating users"
 puts 'Creating 3 fake cities...'
-3.times do
-  city = City.new(
-    name:    Faker::Nation.capital_city
-  )
-  city.save!
-end
-puts 'Finished!'
+# 3.times do
+#   city = City.new(
+#     name:    Faker::Nation.capital_city
+#   )
+#   city.save!
+# end
+
+City.create(name: "Valencia")
+City.create(name: "Madrid")
+City.create(name: "Barcelona")
+
+puts 'Done creating cities!'
 
 puts 'Creating 10 fake jets...'
 10.times do
@@ -31,4 +40,7 @@ puts 'Creating 10 fake jets...'
   )
   jet.save!
 end
-puts 'Finished!'
+puts 'Done creating jets!'
+
+puts 'Seeding ended'
+puts '*' * 30
