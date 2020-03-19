80d52ea649bf922a74079721b64840e28b15c581
Sang Soo Ra <nadang82@gmail.com>
2020-03-19 20:16:48 +0100

    routes clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  patch 'bookings/:id/send', to: 'bookings#send', as: :send
-  patch 'bookings/:id/accept', to: 'bookings#accept', as: :accept
-  patch 'bookings/:id/decline', to: 'bookings#decline', as: :decline
-  patch 'bookings/:id/cancel', to: 'bookings#cancel', as: :cancel
-  devise_for :users
   root to: 'jets#index'
   resources :jets, except: :index do
     resources :bookings, only: [:new, :create]
