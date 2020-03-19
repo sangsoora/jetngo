f2b4fea5c9db69bd1d26654bb6ea019fa8566d03
Abder Nadifi <nabder@naditel.com>
2020-03-19 16:55:44 +0100

    change conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_140125) do
+ActiveRecord::Schema.define(version: 2020_03_19_102800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
