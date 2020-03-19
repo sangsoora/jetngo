5efc92819c4b46d44c5f945fbed67e95087be3e5
Abder Nadifi <nabder@naditel.com>
2020-03-19 17:18:06 +0100

    Change Search name from Find movie to Find a jet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_102800) do
+ActiveRecord::Schema.define(version: 2020_03_19_140125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_102800) do
     t.bigint "jet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status"
+    t.string "status"
     t.index ["jet_id"], name: "index_bookings_on_jet_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
