b72f0bbd5bc481c8aab434d7227ed938df7927a7
Abder Nadifi <nabder@naditel.com>
2020-03-19 15:32:51 +0100

    Search is working with model and name

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'faker'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'cloudinary', '~> 1.12.0'
 gem 'geocoder'
+gem 'pg_search', '~> 2.3.0'
 
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.12.0'
--- app/models/jet.rb
+++ app/models/jet.rb
@@ -6,4 +6,14 @@ class Jet < ApplicationRecord
   validates :capacity, presence: true, numericality: true
   validates :unit_price, presence: true, numericality: true
   has_many_attached :photos
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_model,
+    against: [ :model ],
+    associated_against: {
+      city: [ :name ]
+    },
+    using: {
+      tsearch: { prefix: true, any_word: true }
+    }
 end
