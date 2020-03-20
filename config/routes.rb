de9d50ed20a213f8a2b986eef0efc10e61072e5f
Sang Soo Ra <nadang82@gmail.com>
2020-03-20 17:43:04 +0100

    .

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   resources :jets, except: :index do
     resources :bookings, only: [:new, :create]
   end
-  resources :bookings, except: [:new, :create]
+  resources :bookings, except: [:new, :create] do
+    resources :reviews, only: [:create]
+  end
   resources :users, only: [:show]
+  resources :reviews, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
